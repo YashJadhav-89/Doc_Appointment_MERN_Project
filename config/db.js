5c35b97f69ef3dc9e9c2d8cdd12a8668dcf09335
YashJadhav-89 <jadhavyash030@gmail.com>
2023-09-21 01:32:11 +0530

    @initial commit
    initial commit

--- config/db.js
+++ config/db.js
@@ -1,13 +1,15 @@
-const mongoose = require('mongoose')
-const colors = require('colors')
+const mongoose = require("mongoose");
+const colors = require("colors");
+const dotenv = require('dotenv');
+dotenv.config({path : "./config/.env"});
 
-const connectDB = async() => {
-    try{
-       await mongoose.connect(process.env.MONGO_URL)
-       console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white);
-    } catch (error) {
-        console.log(`Mongodb Server Issue ${error}`.bgRed.white)
-    }
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white);
+  } catch (error) {
+    console.log(`Mongodb Server Issue ${error}`.bgRed.white);
+  }
 };
 
 module.exports = connectDB;
